281cd99df21e3cb27d12e32801f4490f0bf667b0
Vinicyus <vinicyus346@gmail.com>
2021-01-23 15:40:40 -0300

    server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -11,12 +11,9 @@ import uploadConfig from '@config/upload';
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
-
 app.use(errors());
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
