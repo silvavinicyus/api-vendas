d57433088127ef6f032537eeefd17ee7a32c1e16
Vinicyus <vinicyus346@gmail.com>
2021-01-27 12:36:06 -0300

    Gerenciamento de Perfis de Usuarios

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 
 const routes = Router();
@@ -14,5 +15,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
 
 routes.use('/password', passwordRouter);
+
+routes.use('/profile', profileRouter);
 
 export default routes;
